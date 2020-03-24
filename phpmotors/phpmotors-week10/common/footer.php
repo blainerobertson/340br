e7389f9cd76261855953c280d09bc16a64de45cd
Robertson <robertsonb@byui.edu>
2020-03-24 11:18:14 -0600

    Old and moldy - removed

--- phpmotors/phpmotors-week10/common/footer.php
+++
@@ -1,5 +0,0 @@
-<p>© PHP Motors, All rights reserved.</p>
-            <p>All images used are believed to be in "Fair Use". Please notify 
-                the author if any are not and they will be removed.</p>
-            <p>Last Updated: 30 March , 2018</p>
-
